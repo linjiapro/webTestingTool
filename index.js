d40080ea947a2cb0ecc872d5ab82fa6c06a27b15
Lin Jia <linjiapro@linjiapro-pro.dhcp.thefacebook.com>
2015-09-23 11:51:39 -0400

    200json 500 error

--- index.js
+++ index.js
@@ -9,10 +9,17 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
-	res.json({success: "it is running"});
+app.get('/200json', function(req, res) {
+	res.json({success: "200 json api"});
+});
+app.get('/500error', function(req, res) {
+  res.status(500);
+  res.send("500 error");
 });
 
+app.get('/', function(req, res) {
+	res.json({success: "app is running"});
+});
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
