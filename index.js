85a12fb889aabe6f390de037bceb4b3c55ca2669
Lin Jia <linjiapro@linjiapro-pro.dhcp.thefacebook.com>
2015-09-23 11:14:01 -0400

    trim

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 3000));
 
 app.use(express.static(__dirname + '/public'));
 
@@ -9,8 +9,8 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function(req, res) {
+	res.json({success: "it is running"});
 });
 
 app.listen(app.get('port'), function() {
